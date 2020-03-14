ba8da84eee8955c5f7af2af4c17c490f68f0e537
Alex Haynes <aheinous@gmail.com>
2020-03-13 18:45:01 -0700

    clean up bs

--- amntPopup.gd
+++ amntPopup.gd
@@ -7,34 +7,9 @@ onready var cancelButton = $Panel/tuiVBox/tuiHBox/cancelButton
 onready var tuiHBox = $Panel/tuiVBox/tuiHBox
 onready var tuiVBox = $Panel/tuiVBox/
 
-
-#func refresh():
-#	if text == null: # not ready yet
-#		return
-#
-#	tuiVBox.charPos = Vector2(1,1)
-#	tuiVBox.refresh()
-#	panel.charSize = tuiVBox.charSize + Vector2(2,2)
-#	print(tuiVBox.charSize, panel.charSize, '<-')
-#	panel.recenter()
-
-
 func _ready():
-	
 	hide()
-
-
-func _on_cancelButton_pressed():
-	pass
-
-
-func _on_okayButton_pressed():
-	pass
 
 
 func _on_text_resized():
 	refresh()
-
-func _on_amntPopup_resized():
-#	panel.recenter()
-	pass
