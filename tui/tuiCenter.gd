649ee3ee6b5b4cdca8d68295ecb194baadde937c
Alex Haynes <aheinous@gmail.com>
2020-03-17 23:27:43 -0700

    Fix gunstore choose-if-you-want-to-buy-or-sell popup not staying centered

--- tui/tuiCenter.gd
+++ tui/tuiCenter.gd
@@ -5,10 +5,8 @@ func _ready():
 	
 func _onResized():
 	setCharSize((rect_size / TUI.cSize).floor())
-	# print('resized: ', charSize, (rect_size / TUI.cSize).floor())
 	
 	
 func _onRefresh():
-#	assert(get_child_count() == 1)
-	var child = get_children()[0]
-	child.charPos = (charSize/2  - child.charSize/2).round()
+	for child in get_children():
+		child.charPos = (charSize/2  - child.charSize/2).round()
