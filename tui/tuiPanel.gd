35fdf2ebf4d81d7b71a1303cc759195234126d37
Alex Haynes <aheinous@gmail.com>
2020-03-21 20:07:19 -0700

    Add tuiPanel padding feature. Not used right now.

--- tui/tuiPanel.gd
+++ tui/tuiPanel.gd
@@ -1,6 +1,6 @@
 extends "res://tui/tuiElement.gd"
 
-
+export var padding : int = 0
 
 func tuiDraw(tui):
 	if not is_visible_in_tree():
@@ -16,21 +16,21 @@ func _onRefresh():
 		print('n childs: (should be 1) ', get_child_count(), ' ', get_path())	
 		return
 	var child = get_children()[0]
-	child.charPos = Vector2(1,1)
+	child.charPos = Vector2.ONE * (padding+1)
 	
 func getMinCharSize():
-	return get_children()[0].getMinCharSize() + Vector2(2,2)
+	return get_children()[0].getMinCharSize() + Vector2.ONE * (padding*2 + 2)
 
 
 func setCharSize(sz):
 	.setCharSize(sz)
-	get_children()[0].setCharSize(sz-Vector2(2,2))
+	get_children()[0].setCharSize(sz-Vector2.ONE * (padding*2 + 2))
 
 
 func setCharHeight(y):
 	.setCharHeight(y)
-	get_children()[0].setCharWidth(y-2)
+	get_children()[0].setCharWidth(y-2*(padding + 1))
 	
 func setCharWidth(x):
 	.setCharWidth(x)
-	get_children()[0].setCharWidth(x-2)
+	get_children()[0].setCharWidth(x-2*(padding + 1))
