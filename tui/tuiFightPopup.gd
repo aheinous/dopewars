93418bd8e9965c6b018b459ce626c6d4acd8367c
Alex Haynes <aheinous@gmail.com>
2020-03-21 18:15:03 -0700

    remove old commented code

--- tui/tuiFightPopup.gd
+++ tui/tuiFightPopup.gd
@@ -1,17 +1,5 @@
 extends "res://tui/tuiPopup.gd"
 
-#signal done
-
-# onready var bitchesCounter = $PanelContainer/VBoxContainer/YouSection/HBoxContainer/BitchesCounter
-# onready var youHealth = $PanelContainer/VBoxContainer/YouSection/HBoxContainer2/HealthProgressBar
-# onready var officerLabel = $PanelContainer/VBoxContainer/CopSection/HBoxContainer/OfficerLabel
-# onready var deputiesCounter = $PanelContainer/VBoxContainer/CopSection/HBoxContainer/DeputiesCounter
-# onready var copHealth = $PanelContainer/VBoxContainer/CopSection/HBoxContainer2/HealthProgressBar
-# onready var text = $PanelContainer/VBoxContainer/Text
-# onready var standFightButton = $PanelContainer/VBoxContainer/HBoxContainer/StandFightButton
-# onready var runDealDrugsButton = $PanelContainer/VBoxContainer/HBoxContainer/RunDealDrugsButton
-
-
 onready var text = $Panel/tuiVBox/text
 onready var standFightButton = $Panel/tuiVBox/tuiHBox/StandFightButton
 onready var runDealDrugsButton = $Panel/tuiVBox/tuiHBox/RunDealDrugsButton
@@ -19,31 +7,6 @@ onready var runDealDrugsButton = $Panel/tuiVBox/tuiHBox/RunDealDrugsButton
 
 func _ready():
 	hide()
-
-
-#func setupAndShow_old():
-#	var bitches = gameModel.stats().getNumBitches()
-#	bitchesCounter.text = "%s bitch%s" % [bitches, "" if bitches==1 else "es"]
-#	youHealth.value = gameModel.stats().health
-#	officerLabel.text = gameModel.curCop()
-#
-#	var deputies = gameModel.numDeputies()
-#	deputiesCounter.text = "%s %s" % [deputies, "deputy" if deputies==1 else "deputies"]
-#
-#	copHealth.value = gameModel.copHealth()
-#
-#	text.text = gameModel.getFightText()
-#
-#	if gameModel.fightOver():
-#		runDealDrugsButton.text = "View Highscores" if gameModel.gameFinished() else "Deal Drugs"
-#		standFightButton.hide()
-#	else:
-#		standFightButton.text = "Fight" if gameModel.canFight() else "Stand"
-#		standFightButton.show()
-#		runDealDrugsButton.text = "Run"
-#
-#	show()
-
 
 
 func setupAndShow():
